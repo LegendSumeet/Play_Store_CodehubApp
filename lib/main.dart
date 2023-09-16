b58c28624cbc838eb4df0da585433da812f95e56
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-09-16 22:47:13 +0530

    tg

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
               theme: ThemeData(
                 primarySwatch: Colors.grey,
               ),
-              home: SignUp());
+              home: const SignUp());
         });
   }
 }
