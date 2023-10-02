6fc3a762984c13b6ca27040411976b689acc233b
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-10-02 19:21:03 +0530

    added

--- lib/ui/pages/courses/page.dart
+++ lib/ui/pages/courses/page.dart
@@ -7,102 +7,93 @@ class CoursePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final List<VideoSource> videoSources = [
-    VideoSource("Video Title 1", "Video Description 1", "Video URL 1"),
-    VideoSource("Video Title 2", "Video Description 2",
-        "https://www.youtube.com/watch?v=BsnCpESUEqM"),
-  ];
+      VideoSource("Video Title 1", "Video Description 1", "Video URL 1"),
+      VideoSource("Video Title 2", "Video Description 2",
+          "https://www.youtube.com/watch?v=BsnCpESUEqM"),
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Get.back();
-          },
-          icon: const Icon(Icons.arrow_back_ios_new_rounded,color: Colors.white,),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        elevation: 0,
-      ),
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: SafeArea(
-          child: ListView(
-            children: [
-              // Banner Section
-              Container(
-                height: 200,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        'https://e1.pxfuel.com/desktop-wallpaper/606/964/desktop-wallpaper-online-courses-online-learning.jpg'),
-                    fit: BoxFit.cover,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://e1.pxfuel.com/desktop-wallpaper/606/964/desktop-wallpaper-online-courses-online-learning.jpg'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-
-              const SizedBox(height: 20),
-
-              const Text(
+                Positioned(
+                  top: 16,
+                  left: 16,
+                  child: IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
                 "LEARN ALL ABOUT FLUTTER FROM ZERO TO ADVANCE",
-                textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-
-              const SizedBox(height: 20),
-
-              Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Colors.grey[900],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  "Flutter Course",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                "Flutter Course",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-
-              const Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  "Videos to Watch",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                "Videos to Watch",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
-
-              // Improved Video List
-              Column(
-                children: videoSources
-                    .asMap()
-                    .entries
-                    .map(
-                      (entry) => buildVideoCard(entry.value, entry.key),
-                    )
-                    .toList(),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: videoSources.length,
+                itemBuilder: (context, index) {
+                  return buildVideoCard(videoSources[index]);
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget buildVideoCard(VideoSource videoSource, int index) {
+  Widget buildVideoCard(VideoSource videoSource) {
     return GestureDetector(
       onTap: () {
         Get.to(() => VideoPlayer(URLL: videoSource.videoUrl));
@@ -112,7 +103,7 @@ class CoursePage extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
         ),
         elevation: 3,
-        margin: const EdgeInsets.symmetric(vertical: 10),
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
         color: Colors.grey[800],
         child: ListTile(
           leading: const Icon(
